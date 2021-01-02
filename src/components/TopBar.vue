1bcea81537c17ced2f8ad193082c8c7130606bc1
gianriyanto <li.griyanto@gmail.com>
2021-01-02 23:28:19 +1100

    Created TopBar.vue basic layout with logo and name card button.

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,7 +1,14 @@
 <template>
-<div id="TopBar">
+  <div id="TopBar">
+    <span class="logo">
 
-</div>
+    </span>
+    <button class="mycard-button">
+      <span class="button-label">
+        Take my Card
+      </span>
+    </button>
+  </div>
 </template>
 
 <script>
@@ -12,8 +19,38 @@ export default {
 
 <style lang="scss" scoped>
 
-#TopTav {
+#TopBar{
+  width: 100%;
+  height: 70px;
+  border: thin solid darkorange;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 
+  .logo{
+    width: 25px;
+    height: 50px;
+    background-color: #1f1f1f;
+    border-bottom-left-radius: 25px;
+    border-top-left-radius: 25px;
+  }
+
+  .mycard-button{
+    text-decoration: none;
+    outline: none;
+    height: 46px;
+    width: 120px;
+    background-color: transparent;
+    border: thin solid black;
+    border-radius: 9px;
+
+    .button-label{
+      font-family: "Bw Modelica Medium", serif;
+      font-size: 12px;
+      color: black;
+    }
+  }
 }
 
 </style>
