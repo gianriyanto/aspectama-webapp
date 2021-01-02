fa8f328d72cb6e4c902fdfcd6c9e2dfddefde9b4
gianriyanto <li.griyanto@gmail.com>
2021-01-03 01:21:45 +1100

    Created contact bar in ContactBar.vue

--- src/components/ContactBar.vue
+++ src/components/ContactBar.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="ContactBar">
-
+    <div class="bar-container">
+      <span class="input-label">
+        Your contact phone or email
+      </span>
+      <button class="send-button">
+        <span class="button-label">
+          Keep in touch
+        </span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -13,9 +22,47 @@ export default {
 <style lang="scss" scoped>
 
 #ContactBar{
-  width: 60%;
+  display: flex;
+  justify-content: center;
+  width: 450px;
   height: 80px;
-  border: thin solid blueviolet;
+
+  .bar-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 33px;
+    width: 100%;
+    border-radius: 30px;
+    padding: 5px;
+    align-items: center;
+    background-color: white;
+    box-shadow: 0 5px 20px 1px rgba(77, 79, 109, 0.25);
+
+    .input-label{
+      font-family: "Bw Modelica Medium", serif;
+      font-size: 12px;
+      color: lightgrey;
+      letter-spacing: 0;
+      padding-left: 15px
+    }
+    .send-button{
+      text-decoration: none;
+      outline: none;
+      border: 0;
+      height: 33px;
+      width: 110px;
+      background-color: #1f1f1f;
+      border-radius: 32px;
+      cursor: pointer;
+
+      .button-label{
+        font-family: "Bw Modelica Medium", serif;
+        font-size: 10px;
+        color: white;
+      }
+    }
+  }
 }
 
 </style>
