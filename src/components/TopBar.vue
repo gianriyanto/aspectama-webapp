83179fbab712561678f3c70242e347ef35d81ca3
gianriyanto <li.griyanto@gmail.com>
2021-01-03 14:13:17 +1100

    Minor Layout adjustments in TopBar.vue

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,7 +3,7 @@
     <span class="logo"></span>
     <button class="myCard-button">
       <span class="button-label">
-        Take my Card
+        Take My Card
       </span>
     </button>
   </div>
@@ -36,8 +36,8 @@ export default {
   .myCard-button{
     text-decoration: none;
     outline: none;
-    height: 45px;
-    width: 130px;
+    height: 40px;
+    width: 120px;
     background-color: transparent;
     border: thin solid #1f1f1f;
     border-radius: 9px;
@@ -45,7 +45,7 @@ export default {
 
     .button-label{
       font-family: "Bw Modelica Medium", serif;
-      font-size: 12px;
+      font-size: 11px;
       color: #1f1f1f;
     }
   }
