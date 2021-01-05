8b6124b8c102f2688e5d7992483205d7d4941415
gianriyanto <li.griyanto@gmail.com>
2021-01-05 13:33:52 +1100

    Styling changes to tiles in MosaicTiles.vue

--- src/components/MosaicTiles.vue
+++ src/components/MosaicTiles.vue
@@ -9,14 +9,14 @@
         </span>
         <span class="card-1">
           <span class="label">
-            Lorem Ipsum
+            Lorem ipsum dolor sit amet
           </span>
         </span>
       </div>
       <div class="right-side">
         <span class="card-2">
           <span class="label">
-            Lorem Ipsum
+            Lorem ipsum dolor sit amet
           </span>
         </span>
       </div>
@@ -25,19 +25,19 @@
       <div class="top-side">
         <span class="card-3">
           <span class="label">
-            Lorem Ipsum
+            Lorem ipsum dolor sit amet
           </span>
         </span>
         <span class="card-4">
           <span class="label">
-            Lorem Ipsum
+            Lorem ipsum
           </span>
         </span>
       </div>
       <div class="bottom-side">
         <span class="card-5">
           <span class="label">
-            Lorem Ipsum
+            Lorem ipsum dolor sit amet
           </span>
         </span>
       </div>
@@ -73,8 +73,8 @@ export default {
       .card-0{
         height: 37%;
         padding: 20px;
-        margin: 0 3px 3px 0;
-        border-radius: 5px;
+        margin: 0 4px 4px 0;
+        border-radius: 8px;
         box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.25 );
         background-image: linear-gradient(to right top, #97d1f8, #94c7fd, #99bcff);
 
@@ -87,8 +87,8 @@ export default {
       .card-1{
         height: 63%;
         padding: 20px;
-        margin: 3px 3px 0 0;
-        border-radius: 7px;
+        margin: 4px 4px 0 0;
+        border-radius: 8px;
         box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.35 );
         background-image: linear-gradient(to right top, #777cee, #857ceb, #927de8, #9c7ee4);
 
@@ -105,10 +105,10 @@ export default {
       width: 48%;
 
       .card-2{
-        margin: 0 3px 0 3px;
+        margin: 0 4px 0 4px;
         padding: 20px;
         width: 100%;
-        border-radius: 7px;
+        border-radius: 8px;
         background-image: linear-gradient(to right top, #b9a6ee, #efb0da, #f6b1bc, #fac3b1, #ffda99);
 
         .label{
@@ -131,10 +131,10 @@ export default {
       height: 53%;
 
       .card-3{
-        margin: 0 3px 3px 3px;
+        margin: 0 4px 4px 4px;
         padding: 20px;
         width: 60%;
-        border-radius: 7px;
+        border-radius: 8px;
         box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.15 );
         background-image: linear-gradient(to right top, #7e94ff, #898eff, #9687ff, #a380ff, #b177fd);
 
@@ -145,12 +145,12 @@ export default {
         }
       }
       .card-4{
-        margin: 0 0 3px 3px;
+        margin: 0 0 4px 4px;
         padding: 20px;
         width: 40%;
-        border-radius: 7px;
+        border-radius: 8px;
         box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.15 );
-        background-image: linear-gradient(to right top, #fd8f8f, #ff96a9, #ff9fc4, #ffaadd, #fdb7f4);
+        background-image: linear-gradient(to right top, #fd8f8f, #ff96a9, #f5a1c1, #f5abd7);
 
         .label{
           font-family: "Bw Modelica Bold", serif;
@@ -163,10 +163,10 @@ export default {
       height: 47%;
 
       .card-5{
-        margin: 3px 0 0 3px;
+        margin: 4px 0 0 4px;
         padding: 20px;
         width: 100%;
-        border-radius: 7px;
+        border-radius: 8px;
         box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
         background-image: linear-gradient(to right top, #91d6b5, #95dadd, #81c6e2);
 
