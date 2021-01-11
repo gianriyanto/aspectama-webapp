ccdcd20e8c42a07ec52dbe787c91de75ff656ed1
gianriyanto <li.griyanto@gmail.com>
2021-01-12 00:02:47 +1100

    Installed and added font-awesome icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faInstagram, faFacebook, faWhatsapp} from '@fortawesome/free-brands-svg-icons'
+import { faGlobe, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faInstagram, faFacebook, faWhatsapp, faGlobe, faEnvelope);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false
 
 new Vue({
