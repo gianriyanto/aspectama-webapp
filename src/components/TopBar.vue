8fc9869f65b8563427b5a796b7bb1570482cc3fa
gianriyanto <li.griyanto@gmail.com>
2021-01-23 11:48:11 +1100

    Added responsiveness in TopBar.vue

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -80,4 +80,61 @@ export default {
   }
 }
 
+@media only screen and (max-width: 768px) {
+  #TopBar{
+    width: 95%;
+    height: 75px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    .logo-container{
+      padding-top: 3px;
+      cursor: pointer;
+
+      .logo-image{
+        height: 55px;
+        width: auto;
+      }
+    }
+
+    .myCard-button{
+      display: none;
+      height: 42px;
+      width: 120px;
+      background-color: transparent;
+      border: thin solid #6e6e6e;
+      border-radius: 9px;
+      position: relative;
+
+      .button-label{
+        position: relative;
+        font-family: "Bw Modelica Bold", serif;
+        font-size: 12px;
+        color: #353535;
+        z-index: 2;
+      }
+      &:after {
+        position: absolute;
+        content: '';
+        top: 0;
+        left: 0;
+        width: 0;
+        height: 100%;
+        background: linear-gradient(to right, #c8dffd, #c7d1ff);
+        border-radius: 9px;
+        transition: all 0.35s;
+      }
+      &:hover {
+        color: #fff;
+        opacity: 1;
+      }
+      &:hover:after {
+        width: 100%;
+      }
+    }
+  }
+}
+
 </style>
