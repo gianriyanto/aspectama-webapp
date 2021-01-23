abb125f9ebee5ac33c16f1d5786bf17644c12813
gianriyanto <li.griyanto@gmail.com>
2021-01-23 12:04:31 +1100

    Added responsiveness in BottomBar.vue.vue

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,7 +1,13 @@
 <template>
-  <div id="BottomBar">
-    <SocialLinks/>
-    <ContactDetails/>
+  <div>
+    <div id="BottomBar">
+      <SocialLinks/>
+      <ContactDetails/>
+    </div>
+    <div id="BottomBarMobile">
+      <SocialLinks/>
+      <ContactDetails/>
+    </div>
   </div>
 </template>
 
@@ -19,20 +25,51 @@ export default {
 
 <style lang="scss" scoped>
 
-#BottomBar{
-  width: 95%;
-  height: 30px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+@media only screen and (min-width: 768px) {
+  #BottomBar {
+    width: 95%;
+    height: 30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 
-  #SocialLinks {
-    width: 90px;
+    #SocialLinks {
+      width: 90px;
+    }
+
+    #ContactDetails {
+      width: 290px;
+    }
   }
 
-  #ContactDetails{
-    width: 290px;
+  #BottomBarMobile {
+    display: none;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+
+  #BottomBar{
+    display: none;
+  }
+
+  #BottomBarMobile {
+    width: 95%;
+    height: 30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border: thin solid hotpink;
+
+    #SocialLinks {
+      width: 90px;
+    }
+
+    #ContactDetails {
+      width: 290px;
+    }
   }
 }
 
